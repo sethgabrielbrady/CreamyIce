52f4d70dff54122fafe34984ec47c9561c43289a
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-02-20 12:25:08 -0500

    Corrections to Creamy Ice homework

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var color1 = function(){
+    document.getElementById("choco").style.backgroundColor = "rgb(134,68,20)";
+};
+var color2 = function(){
+    document.getElementById("vanilla").style.backgroundColor = "rgb(252,251,227)";
+};
+var color3 = function(){
+    document.getElementById("fruity").style.backgroundColor = "rgb(121,17,61)";
+};
+var color4 = function(){
+    document.getElementById("pistachio").style.backgroundColor = "rgb(156,185,46)";
+};
+var color5 = function(){
+    document.getElementById("caramel").style.backgroundColor = "rgb(181,133,31)";};
+
